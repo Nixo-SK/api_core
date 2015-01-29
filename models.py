2b047ce5613452041f46b51aa447c6f6f9de4d1c
Dusan Matejka <matejkadusan32@gmail.com>
2015-01-29 16:55:24 +0100

    added model for simple token authentication

--- models.py
+++ models.py
@@ -1,3 +1,30 @@
+from django.utils import timezone
 from django.db import models
+from uuid import uuid4
 
-# Create your models here.
+
+class SimpleTokenAuthModel(models.Model):
+    token_uuid = models.CharField(max_length=36, unique=True, default=str(uuid4()))
+    token_last_use = models.DateTimeField(default=timezone.now())
+    token_enabled = models.BooleanField(default=True)
+
+    def get_token(self):
+        if self.token_enabled:
+            self.token_last_use = timezone.now()
+            return self
+        else:
+            return None
+
+    def change_token_status(self, data):
+        if data.lower() == 'true':
+            self.token_enabled = True
+        elif data.lower() == 'false':
+            self.token_enabled = False
+        else:
+            return False
+
+        return True
+
+    def is_authenticated(self):
+        return True
+
