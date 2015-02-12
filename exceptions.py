893226192ad227425c714e93a1b3ce54e4384e22
Dusan Matejka <matejkadusan32@gmail.com>
2015-02-12 13:45:59 +0000

    Created DoesNotExist and DuplicateEntry error responses

--- exceptions.py
+++ exceptions.py
@@ -4,3 +4,13 @@ from rest_framework.exceptions import APIException
 class InvalidRequestException(APIException):
     status_code = 400
     default_detail = 'Invalid request data'
+
+
+class DoesNotExistException(APIException):
+    status_code = 404
+    default_detail = 'Requested data does not exist'
+
+
+class DuplicateEntryException(APIException):
+    status_code = 400
+    default_detail = 'Duplicate entry.'
